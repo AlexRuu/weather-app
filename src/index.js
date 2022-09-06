eb253f9b295f93966b45ef3df197b3625a6cb01b
Alex <weiru@hotmail.ca>
2022-09-06 16:03:35 -0400

    Reset input field when submitted

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import getWeather from "./getWeather";
-import update from "./Update"
+import update from "./update"
 
-update(getWeather('Toronto'));
+window.onload = update(getWeather("Toronto"))
+
+const submit = document.querySelector("#submit");
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    let search = document.forms[0]
+    let city = search["city"].value
+    update(getWeather(city));
+    search.reset();
+})
